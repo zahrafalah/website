ad14e6075e230cc3b4bc372aa837e9ea6d104034
Sepideh <sepidehfallah2@gmail.com>
2019-08-08 12:29:41 -0700

    testing products routings through postman app, populating the product table wsith seeds and getting real time daa ba data

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 import HomepageLayout from './components/home/HomePageLayout';
-import Users from './components/users/users';
-import Products from './components/products/products';
+import Users from './components/users/Users';
+import Products from './components/products/Products';
 
 function App() {
   return (
